6dc06ad312b6ab7a0306c2cd557ac70c2cbdb89e
sh.monzavi <sh.monzavi@MONZAVI-PC>
2024-03-25 10:48:50 +0330

    Add project files.

---
+++ src/Api/Program.cs
@@ -0,0 +1,41 @@
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services
+    .AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.TokenValidationParameters.ValidateAudience = false;
+    });
+
+builder.Services
+       .AddAuthorization(options =>
+       {
+           options.AddPolicy("ApiScope", policy =>
+           {
+               policy.RequireAuthenticatedUser();
+               policy.RequireClaim("scope", "api1");
+           });
+       });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("identity", (ClaimsPrincipal user) =>
+    user.Claims.Select(c => new { c.Type, c.Value }))
+    .RequireAuthorization("ApiScope");
+
+app.Run();
